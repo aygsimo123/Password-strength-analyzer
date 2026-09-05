75b1f40b533c6bfe5504a23e2b96e9803c8d31f0
aygsimo123 <saidtilmirat1@gmail.com>
2026-09-05 20:45:09 +0200

    second version of the password analyzer tool

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 import re 
+import sys
 
 CYAN = "\033[96m"
 RESET = "\033[0m"
+GREEN = "\033[92m"
+RED = "\033[91m"
 
 print(CYAN + "========================================" + RESET)
 print(CYAN + "          PASSWORD STRENGHT ANALYZER" + RESET)
@@ -14,12 +17,21 @@ print(CYAN + "[3] Exit" + RESET + "\n\n ")
 
 option = input(CYAN + "[?] Choose an option : " + RESET)
 
-if bool(re.fullmatch(r"^[123]" , option)) :
-	if option == 1 : 
-		print("1")
-	elif option == 2 :
+if bool(re.fullmatch(r"[123]" , option)) :
+	if option == "1" : 
+		password = input ("Enter the password : ")
+		if bool(re.fullmatch(r"(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}" , password)) :
+			print(GREEN + "[+] Strong password! It meets all security requirements" + RESET)
+		else : 
+		    print(RED + "[-] Weak password! It must contain:" + RESET)
+		    print(RED + "    - At least 8 characters" + RESET)
+		    print(RED + "    - At least one uppercase letter (A-Z)" + RESET)
+	   	    print(RED + "    - At least one lowercase letter (a-z)" + RESET)
+    		    print(RED + "    - At least one number (0-9)" + RESET)
+		    print(RED + "    - At least one special character" + RESET)
+	elif option == "2" :
 		print("2")
-	elif option == 3 :
+	elif option == "3" :
 		sys.exit()
 
 else : 
